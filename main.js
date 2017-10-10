03226c121cf48272eb25e28938e72b7ce7ad1ffe
len4ik1801 <len4ik1801@gmail.com>
2017-10-10 14:18:32 +0300

    fixed functionality bug

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
  //min
-var array = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
+var array1 = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
+    array2 = [-1,-8,-2];
+    array3 = [1,7,3];
+    array4 = [1,undefined,3,5,-3];
+    array5 = [1,NaN,3,5,-3];
 function getMinValue(arr){
     var min = arr[0];
     for (var i = 0; i < arr.length; i++) {
@@ -7,11 +11,15 @@ function getMinValue(arr){
     }
     return min;
 }
-console.log ('arrayMin',getMinValue(array))  //-12\
+console.log ('arrayMin',getMinValue( [array1],[array2], [array3], [array4], [array5] ))  //-12\
 
 
 //max
 var array = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
+    array2 = [-1,-8,-2];
+    array3 = [1,7,3];
+    array4 = [1,undefined,3,5,-3];
+    array5 = [1,NaN,3,5,-3];
 function getMaxValue (array) {
 var max = array [0];
 for (var i = 0; i < array.length; i++){
@@ -19,11 +27,15 @@ if (max < array [i]) max = array[i];
     }
 return max;
     }
-console.log ('arrayMax',getMaxValue(array))  //44\
+console.log ('arrayMax',getMaxValue( [array1], [array2], [array3], [array4], [array5] ))  //44\
 
 
 //sum
-var arr = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
+var array = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
+    array2 = [-1,-8,-2];
+    array3 = [1,7,3];
+    array4 = [1,undefined,3,5,-3];
+    array5 = [1,NaN,3,5,-3];
 function arraySum(array){
 var sum = 0;
 for(var i = 0; i < array.length; i++){
@@ -31,144 +43,144 @@ for(var i = 0; i < array.length; i++){
     }
 return sum;
 }
-console.log('arraySum', arraySum(arr)); //32\
+console.log('arraySum', arraySum( [array1], [array2], [array3], [array4], [array5] )); //32\
 
- //min
-var array = [-1,-8,-2];
-function getMinValue(arr){
-    var min = arr[0];
-    for (var i = 0; i < arr.length; i++) {
-        if (min > arr[i]) min = arr[i];
-    }
-    return min;
-}
-console.log ('arrayMin',getMinValue(array))  //-8\
+//  //min
+// var array = [-1,-8,-2];
+// function getMinValue(arr){
+//     var min = arr[0];
+//     for (var i = 0; i < arr.length; i++) {
+//         if (min > arr[i]) min = arr[i];
+//     }
+//     return min;
+// }
+// console.log ('arrayMin',getMinValue(array))  //-8\
 
- //max
-var array = [-1,-8,-2];
-function getMaxValue (array) {
-var max = array [0];
-for (var i = 0; i < array.length; i++){
-if (max < array [i]) max = array[i];
-    }
-return max;
-    }
-console.log ('arrayMax',getMaxValue(array))  //-1\
+//  //max
+// var array = [-1,-8,-2];
+// function getMaxValue (array) {
+// var max = array [0];
+// for (var i = 0; i < array.length; i++){
+// if (max < array [i]) max = array[i];
+//     }
+// return max;
+//     }
+// console.log ('arrayMax',getMaxValue(array))  //-1\
+//
 
-
- //sum
-var arr = [-1,-8,-2];
-function arraySum(array){
-var sum = 0;
-for(var i = 0; i < array.length; i++){
-    sum += array[i];
-    }
-return sum;
-}
-console.log('arraySum',arraySum(arr)); //-11\
-
-
-//min
-var array = [1,7,3];
-function getMinValue(arr){
-    var min = arr[0];
-    for (var i = 0; i < arr.length; i++) {
-        if (min > arr[i]) min = arr[i];
-    }
-    return min;
-}
-console.log ('arrayMin',getMinValue(array))  //1\
-
-//max
-var array = [1,7,3];
-function getMaxValue (array) {
-var max = array [0];
-for (var i = 0; i < array.length; i++){
-if (max < array [i]) max = array[i];
-    }
-return max;
-    }
-console.log ('arrayMax',getMaxValue(array)) //7\
-
-//sum
-var arr = [1,7,3];
-function arraySum(array){
-var sum = 0;
-for(var i = 0; i < array.length; i++){
-    sum += array[i];
-    }
-return sum;
-}
-console.log('arraySum',arraySum(arr)) //11\
+//  //sum
+// var arr = [-1,-8,-2];
+// function arraySum(array){
+// var sum = 0;
+// for(var i = 0; i < array.length; i++){
+//     sum += array[i];
+//     }
+// return sum;
+// }
+// console.log('arraySum',arraySum(arr)); //-11\
 
 
-//min
-var array = [1,undefined,3,5,-3];
-function getMinValue(arr){
-    var min = arr[0];
-    for (var i = 0; i < arr.length; i++) {
-        if (min > arr[i]) min = arr[i];
-    }
-    return min;
-}
-console.log ('arrayMin',getMinValue(array))
-
-//max
-
-var array = [1,undefined,3,5,-3];
-function getMaxValue (array) {
-var max = array [0];
-for (var i = 0; i < array.length; i++){
-if (max < array [i]) max = array[i];
-    }
-return max;
-    }
-console.log ('arrayMax',getMaxValue(array))  //5\
-
-
-//sum
-var arr = [1,undefined,3,5,-3];
-function arraySum(array){
-var sum = 0;
-for(var i = 0; i < array.length; i++){
-    sum += array[i];
-    }
-return sum;
-}
-console.log('arraySum',arraySum(arr))  // NaN\
-
-
-//min
-var array = [1,NaN,3,5,-3];
-function getMinValue(arr){
-    var min = arr[0];
-    for (var i = 0; i < arr.length; i++) {
-        if (min > arr[i]) min = arr[i];
-    }
-    return min;
-}
-console.log ('arrayMin',getMinValue(array)) //-3\
-
-
-//max
-var array = [1,NaN,3,5,-3];
-function getMaxValue (array) {
-var max = array [0];
-for (var i = 0; i < array.length; i++){
-if (max < array [i]) max = array[i];
-    }
-return max;
-    }
-console.log ('arrayMax',getMaxValue(array)) //5\
-
-
-//sum
-var arr = [1,NaN,3,5,-3];
-function arraySum(array){
-var sum = 0;
-for(var i = 0; i < array.length; i++){
-    sum += array[i];
-    }
-return sum;
-}
-console.log('arraySum',arraySum(arr)) // NaN
+// //min
+// var array = [1,7,3];
+// function getMinValue(arr){
+//     var min = arr[0];
+//     for (var i = 0; i < arr.length; i++) {
+//         if (min > arr[i]) min = arr[i];
+//     }
+//     return min;
+// }
+// console.log ('arrayMin',getMinValue(array))  //1\
+//
+// //max
+// var array = [1,7,3];
+// function getMaxValue (array) {
+// var max = array [0];
+// for (var i = 0; i < array.length; i++){
+// if (max < array [i]) max = array[i];
+//     }
+// return max;
+//     }
+// console.log ('arrayMax',getMaxValue(array)) //7\
+//
+// //sum
+// var arr = [1,7,3];
+// function arraySum(array){
+// var sum = 0;
+// for(var i = 0; i < array.length; i++){
+//     sum += array[i];
+//     }
+// return sum;
+// }
+// console.log('arraySum',arraySum(arr)) //11\
+//
+//
+// //min
+// var array = [1,undefined,3,5,-3];
+// function getMinValue(arr){
+//     var min = arr[0];
+//     for (var i = 0; i < arr.length; i++) {
+//         if (min > arr[i]) min = arr[i];
+//     }
+//     return min;
+// }
+// console.log ('arrayMin',getMinValue(array))
+//
+// //max
+//
+// var array = [1,undefined,3,5,-3];
+// function getMaxValue (array) {
+// var max = array [0];
+// for (var i = 0; i < array.length; i++){
+// if (max < array [i]) max = array[i];
+//     }
+// return max;
+//     }
+// console.log ('arrayMax',getMaxValue(array))  //5\
+//
+//
+// //sum
+// var arr = [1,undefined,3,5,-3];
+// function arraySum(array){
+// var sum = 0;
+// for(var i = 0; i < array.length; i++){
+//     sum += array[i];
+//     }
+// return sum;
+// }
+// console.log('arraySum',arraySum(arr))  // NaN\
+//
+//
+// //min
+// var array = [1,NaN,3,5,-3];
+// function getMinValue(arr){
+//     var min = arr[0];
+//     for (var i = 0; i < arr.length; i++) {
+//         if (min > arr[i]) min = arr[i];
+//     }
+//     return min;
+// }
+// console.log ('arrayMin',getMinValue(array)) //-3\
+//
+//
+// //max
+// var array = [1,NaN,3,5,-3];
+// function getMaxValue (array) {
+// var max = array [0];
+// for (var i = 0; i < array.length; i++){
+// if (max < array [i]) max = array[i];
+//     }
+// return max;
+//     }
+// console.log ('arrayMax',getMaxValue(array)) //5\
+//
+//
+// //sum
+// var arr = [1,NaN,3,5,-3];
+// function arraySum(array){
+// var sum = 0;
+// for(var i = 0; i < array.length; i++){
+//     sum += array[i];
+//     }
+// return sum;
+// }
+// console.log('arraySum',arraySum(arr)) // NaN
