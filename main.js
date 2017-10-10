3a8865a354f10b22434b3b3fbcb342da4b12b4d3
len4ik1801 <len4ik1801@gmail.com>
2017-10-10 14:45:41 +0300

    removed unnecessary variables

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
- //min
 var array1 = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
 var array2 = [-1,-8,-2];
 var array3 = [1,7,3];
 var array4 = [1,undefined,3,5,-3];
 var array5 = [1,NaN,3,5,-3];
+
+ //min
 function getMinValue(arr){
     var min = arr[0];
     for (var i = 0; i < arr.length; i++) {
@@ -20,11 +21,6 @@ console.log ('arrayMin',getMinValue( array5));
 
 
 //max
-var array = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
-var array2 = [-1,-8,-2];
-var array3 = [1,7,3];
-var array4 = [1,undefined,3,5,-3];
-var array5 = [1,NaN,3,5,-3];
 function getMaxValue (array) {
 var max = array [0];
 for (var i = 0; i < array.length; i++){
@@ -40,11 +36,6 @@ console.log ('arrayMax',getMaxValue(array5));
 
 
 //sum
-var array = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
-var array2 = [-1,-8,-2];
-var array3 = [1,7,3];
-var array4 = [1,undefined,3,5,-3];
-var array5 = [1,NaN,3,5,-3];
 function arraySum(array){
 var sum = 0;
 for(var i = 0; i < array.length; i++){
